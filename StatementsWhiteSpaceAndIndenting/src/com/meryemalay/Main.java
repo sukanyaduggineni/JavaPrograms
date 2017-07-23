09cd0ac16a1c9e83ca137e3cc74159c65fb3e684
meryem <meryemalay@gmail.com>
2017-07-23 13:46:47 +0100

    Statements White Space and Indenting

--- StatementsWhiteSpaceAndIndenting/src/com/meryemalay/Main.java
+++ StatementsWhiteSpaceAndIndenting/src/com/meryemalay/Main.java
@@ -3,7 +3,25 @@ package com.meryemalay;
 public class Main {
 
     public static void main(String[] args) {
-        // the complete line is statement
+        // the complete line is a valid Java statement
         int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+        //Semicolon is needed to complete a Java line, to make it a statement.
+
+        System.out.println("This is " +
+                "another " +
+                "still more.");
+
+        int anotherVariable = 50;
+        myVariable--;
+        System.out.println("This is another one");
+
+        //indenting
+        //Format: go up to the Code menu, select Reformant Code
+        //shortcut: alt + cmd + L
+
+
     }
 }
