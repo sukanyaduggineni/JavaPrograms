bf5e43fca258b1042a844e3068d7bb1716160257
meryem <meryemalay@gmail.com>
2017-08-07 23:09:21 +0100

    Autobaxing and Unboxing Challenge part 1

---
+++ Banking/src/com/meryemalay/Bank.java
@@ -0,0 +1,8 @@
+package com.meryemalay;
+
+import java.util.ArrayList;
+
+public class Bank {
+
+    ArrayList<Branch> branches = new ArrayList<Branch>();
+}
--- Banking/src/com/meryemalay/Main.java
+++ Banking/src/com/meryemalay/Main.java
@@ -3,6 +3,6 @@ package com.meryemalay;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
