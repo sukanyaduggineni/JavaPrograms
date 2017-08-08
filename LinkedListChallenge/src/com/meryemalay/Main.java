baf7acbb5aab9640872d09fa21626417fc804448
meryem <meryemalay@gmail.com>
2017-08-08 13:13:06 +0100

    LinkedList Challenge part1

---
+++ LinkedListChallenge/src/com/meryemalay/Main.java
@@ -0,0 +1,18 @@
+package com.meryemalay;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static void printMenu(){
+        System.out.println("0 - quit\n" +
+                        "1 - to skip next song\n" +
+                        "2 - to skip previous song\n" +
+                        "3 - to replay song\n" +
+                        "4 - to list all songs in the play list\n" +
+                        "5 - to remove the current song\n" +
+                        "6 - to print menu");
+    }
+}
