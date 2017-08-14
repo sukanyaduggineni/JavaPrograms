88cfa2c27a2bfecccad685ae536ec8eafff3bef0
meryem <meryemalay@gmail.com>
2017-08-14 18:07:06 +0100

    Packages Challenge

---
+++ SeriesTest/src/com/example/series/test/Main.java
@@ -0,0 +1,21 @@
+package com.example.series.test;
+
+import com.meryemalay.libraries.Series;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10 ; i++) {
+            System.out.println(Series.nSum(i));
+        }
+        System.out.println("--------------------------");
+        for (int i = 0; i <=10 ; i++) {
+            System.out.println(Series.factorial(i));
+        }
+        System.out.println("--------------------------");
+
+        for (int i = 0; i <=10 ; i++) {
+            System.out.println(Series.fibonacci(i));
+        }
+    }
+}
