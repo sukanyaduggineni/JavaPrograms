a6ea668f399485a3e2d58db88f915b0b63880312
meryem <meryemalay@gmail.com>
2017-08-23 16:09:42 +0100

    Collections number seven Adventure Game Exits

---
+++ Collections/src/com/meryemalay/MapProgram.java
@@ -0,0 +1,52 @@
+package com.meryemalay;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapProgram {
+
+    public static void main(String[] args) {
+        Map<String, String> languages = new HashMap<>();
+        if(languages.containsKey("Java")){
+            System.out.println("Java already exist");
+        } else {
+            languages.put("Java", "a compiled high level, object-oriented, platform independent language");
+            System.out.println("Java added successfully");
+        }
+
+        languages.put("Python", "an interpreted, object-oriented, high-level programming language with dynamic semantics");
+        languages.put("Algol", "an algorithmic language");
+        System.out.println(languages.put("BASIC", "Beginners All Purpose Symbolic Instruction Code"));
+        System.out.println(languages.put("Lisp", "Therein lies madness"));
+
+        if(languages.containsKey("Java")){
+            System.out.println("Java is already in the map");
+        }else{
+            languages.put("Java", "This course is about Java");
+        }
+
+        System.out.println("===============================");
+
+        //System.out.println(languages.remove("Lisp"));
+        if(languages.remove("Algol", "an algorithmic language")){
+            System.out.println("Algol removed");
+        }else{
+            System.out.println("Algol not removed, key/value pair not found");
+        }
+
+        //System.out.println(languages.replace("Lisp","a functional programming with impretaive features"));
+        //System.out.println(languages.replace("Scala","this will not be added"));
+
+        if(languages.replace("Lisp","Therein lies madness","a functional programming with impretaive features")){
+            System.out.println("Lisp replaced");
+        }else{
+            System.out.println("Lisp was not replaced");
+        }
+
+
+        for (String key: languages.keySet()){
+            System.out.println(key + " : " + languages.get(key));
+        }
+
+    }
+}
